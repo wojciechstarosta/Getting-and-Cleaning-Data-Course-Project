645756c9ad6078a7bd048d1596cdb9a0ccd27eb6
wojciechstarosta <rumcajs86@wp.pl>
2017-02-21 14:04:12 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,16 @@
 #uploading features set and activity labels set
-setwd("H:/BOK/Rozwój/R programming/Course 3 Data cleaning/UCI HAR Dataset")
+setwd("./UCI HAR Dataset")
 features<-read.csv2("./features.txt", header = FALSE, col.names=c("features"))
 activity_labels<-read.csv2("./activity_labels.txt", header = FALSE, sep="", col.names=c("activity", "activity.name"))
 
 #uploading training set
-setwd("H:/BOK/Rozwój/R programming/Course 3 Data cleaning/UCI HAR Dataset/train")
+setwd("./UCI HAR Dataset/train")
 data_train_x<-read.csv2("./X_train.txt", header = FALSE, sep = "", col.names=as.matrix(features))
 data_train_y<-read.csv2("./Y_train.txt", header = FALSE, col.names=c("activity.nr"))
 subject_train<-read.csv2("./subject_train.txt", header = FALSE, col.names=c("Subject"))
 
 #uploading test set
-setwd("H:/BOK/Rozwój/R programming/Course 3 Data cleaning/UCI HAR Dataset/test")
+setwd("./UCI HAR Dataset/test")
 data_test_x<-read.csv2("./X_test.txt", header = FALSE, sep = "", col.names=as.matrix(features))
 data_test_y<-read.csv2("./Y_test.txt", header = FALSE, col.names=c("activity.nr"))
 subject_test<-read.csv2("./subject_test.txt", header = FALSE, col.names=c("Subject"))
@@ -36,5 +36,5 @@ data_grouped[data_grouped_numeric] <- lapply(data_grouped[data_grouped_numeric],
 
 #summarizing data
 tidy_data<-data_grouped[,1:81] %>% summarise_all(mean)
-setwd("H:/BOK/Rozwój/R programming/Course 3 Data cleaning/UCI HAR Dataset")
+setwd("./UCI HAR Dataset")
 write.table(tidy_data, file="./tidy_data.txt", row.names=FALSE)
